b692c6c12e6468d053452f5f9c043aa90acff48b
Simone2611 <simone26v@gmail.com>
2025-09-15 16:40:44 +0200

    Added navbar and login without functionallity

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Login } from './login/login';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'login', component: Login }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
