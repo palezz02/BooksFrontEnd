4fe0ffa439c71d3f3af511512cd4bd55ff6f56c5
DESKTOP-62RHKRO\Betacom <g.palezza@gmail.com>
2025-09-15 09:31:50 +0200

    BooksFontEnd init

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -3,10 +3,22 @@ import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { App } from './app';
 import { AppModule } from './app-module';
 import { serverRoutes } from './app.routes.server';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing-module';
 
 @NgModule({
-  imports: [AppModule],
-  providers: [provideServerRendering(withRoutes(serverRoutes))],
+  declarations:[],
+  imports: [
+    AppModule,
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [
+    provideServerRendering(withRoutes(serverRoutes)),
+    provideHttpClient(withFetch()),
+    provideClientHydration(withEventReplay())
+  ],
   bootstrap: [App],
 })
 export class AppServerModule {}
