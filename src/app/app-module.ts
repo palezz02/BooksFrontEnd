2d395e1a5fe77ac80a0134bc250aa27de3af0a8a
Simone2611 <simone26v@gmail.com>
2025-09-18 16:30:12 +0200

    Added cardBook

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -11,21 +11,23 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
+import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Navbar } from './components/navbar/navbar';
 import { Login } from './components/login/login';
 import { Home } from './components/home/home';
+import { CardBook } from './card-book/card-book';
 
 @NgModule({
-  declarations: [App, Navbar, Login, Home],
+  declarations: [App, Navbar, Login, Home, CardBook],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
     MatFormFieldModule,
     MatInputModule,
+    MatCardModule,
     MatButtonModule,
     MatIconModule,
     FormsModule,
