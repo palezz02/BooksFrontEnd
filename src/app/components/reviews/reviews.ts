47bfbc3c6e4a18eb70c35a809c7b6609ec599523
Simone2611 <simone26v@gmail.com>
2025-10-02 14:51:56 +0200

    review work

--- src/app/components/reviews/reviews.ts
+++ src/app/components/reviews/reviews.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReviewService } from '../../services/review-service';
+import { AuthService } from '../../auth/authService';
+import { title } from 'process';
+import { UserService } from '../../services/user-service';
 
 @Component({
   selector: 'app-reviews',
@@ -16,10 +19,16 @@ export class Reviews {
   showReviewForm: boolean = false;
   reviewForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private reviewService: ReviewService) {
+  constructor(
+    private fb: FormBuilder,
+    private reviewService: ReviewService,
+    private userService: UserService,
+    public auth: AuthService // cambia da private a public
+  ) {
     this.reviewForm = this.fb.group({
       title: ['', Validators.required],
       body: ['', Validators.required],
+      rating: [0, [Validators.required, Validators.min(1), Validators.max(5)]],
     });
   }
 
@@ -44,11 +53,43 @@ export class Reviews {
     }
   }
 
+  setRating(star: number) {
+    this.reviewForm.get('rating')?.setValue(star);
+    console.log(star);
+  }
+
   submitReview() {
-    // Da implementare: invio della recensione
-    // Esempio: console.log(this.reviewForm.value);
-    this.reviewService.create(this.reviewForm.value).subscribe((res) => {
-      // Gestisci la risposta positiva
+    const review = {
+      id: 0,
+      bookId: this.reviews[0]?.bookId,
+      userId: this.auth.getUserId(),
+      rating: this.reviewForm.value.rating,
+      title: this.reviewForm.value.title,
+      body: this.reviewForm.value.body,
+      firstName: '', // aggiungi default
+      lastName: '', // aggiungi default
+      reviewTitle: this.reviewForm.value.title,
+    };
+    console.log(review);
+    this.userService.getById(this.auth.getUserId()).subscribe((userRes) => {
+      if (userRes.rc && userRes.dati) {
+        review.firstName = userRes.dati.firstName || '';
+        review.lastName = userRes.dati.lastName || '';
+      }
+      this.reviewService.create(review).subscribe((res) => {
+        console.log('Review submitted successfully', res);
+        // Inserisci la recensione subito dopo la risposta del backend
+        this.reviews = [{ ...review }, ...this.reviews];
+        // Resetta il modulo
+        this.reviewForm.reset({ rating: 0 });
+        // Nascondi il modulo di recensione
+        this.showReviewForm = false;
+        // Torna all'inizio della lista per mostrare la nuova recensione
+        this.currentIndex = 0;
+      });
     });
   }
 }
+
+// Make sure ReviewService, UserService, and AuthService are all decorated with:
+// @Injectable({ providedIn: 'root' })
